82831c08e00b513e6a5783183cf203f0cf46c616
Pavel Patrusov <pavelp9415@gmail.com>
2019-10-04 00:04:11 +0300

    ASP.NET Core Fundamentals - 2.4 - Drilling into Data: Injecting and Using Configuration

--- asp-dotnet-core-path/asp-dotnet-core-fundamentals/module_02/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ asp-dotnet-core-path/asp-dotnet-core-fundamentals/module_02/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -1,16 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration _config;
+
+        public string Message { get; set; }
+
+        public ListModel(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public void OnGet()
         {
+            Message = _config["Message"];
         }
     }
 }
