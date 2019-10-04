3e0251cee4b5706ed6f7094f9e8b3d6205cce25a
Pavel Patrusov <pavelp9415@gmail.com>
2019-10-04 21:04:51 +0300

    ASP.NET Core Fundamentals - 2.7 - Drilling into Data: Building a Data Access Service

---
+++ asp-dotnet-core-path/asp-dotnet-core-fundamentals/module_02/OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Pavel`s Pizza", Location = "Haifa", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.Indian},
+                new Restaurant {Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.Mexican}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
