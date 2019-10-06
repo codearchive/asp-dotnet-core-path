a915984ec32cb2393bf44f732924b896ff6f05be
Pavel Patrusov <pavelp9415@gmail.com>
2019-10-06 20:51:20 +0300

    ASP.NET Core Fundamentals - 3.3 - Working with Models and Model Binding: Building a Search Form

---
+++ asp-dotnet-core-path/asp-dotnet-core-fundamentals/module_02/OdeToFood/OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private readonly List<Restaurant> _restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Pavel`s Pizza", Location = "Haifa", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.Indian},
+                new Restaurant {Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.Mexican}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
