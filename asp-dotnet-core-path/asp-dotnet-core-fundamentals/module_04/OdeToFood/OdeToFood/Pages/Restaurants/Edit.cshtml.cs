4a99ada616f8aef900058d294a1181bb9cc4c599
Pavel Patrusov <pavelp9415@gmail.com>
2019-10-17 23:55:24 +0300

    ASP.NET Core Fundamentals - 4.3 - Editing Data with Razor Pages: Building an Edit Form with Tag Helpers

--- asp-dotnet-core-path/asp-dotnet-core-fundamentals/module_04/OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ asp-dotnet-core-path/asp-dotnet-core-fundamentals/module_04/OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -8,15 +10,20 @@ namespace OdeToFood.Pages.Restaurants
     public class EditModel : PageModel
     {
         private readonly IRestaurantData _restaurantData;
+        private readonly IHtmlHelper _htmlHelper;
 
         public Restaurant Restaurant { get; set; }
 
-        public EditModel(IRestaurantData restaurantData)
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
         {
             this._restaurantData = restaurantData;
+            this._htmlHelper = htmlHelper;
         }
         public IActionResult OnGet(int restaurantId)
         {
+            Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
             Restaurant = _restaurantData.GetById(restaurantId);
             if (Restaurant == null)
             {
