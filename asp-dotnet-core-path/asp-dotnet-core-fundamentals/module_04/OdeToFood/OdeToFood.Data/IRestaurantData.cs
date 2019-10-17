f4c6ed319fae78bcb7204a7a4d86fea359f45a69
Pavel Patrusov <pavelp9415@gmail.com>
2019-10-17 23:35:40 +0300

    ASP.NET Core Fundamentals - 4.2 - Editing Data with Razor Pages: Creating the Restaurant Edit Page

---
+++ asp-dotnet-core-path/asp-dotnet-core-fundamentals/module_04/OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+    }
+}
